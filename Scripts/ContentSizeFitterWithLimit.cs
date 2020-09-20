88aea048a729eca2720ef45ca7254bc2220adf3d
phanphantz <phunphun484@gmail.com>
2020-09-20 12:54:05 +0700

    Add files via upload

---
+++ Scripts/ContentSizeFitterWithLimit.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+[System.Serializable]
+[ExecuteAlways]
+public class ContentSizeFitterWithLimit : ContentSizeFitter
+{
+    private bool limitWidth;
+    private bool limitHeight;
+    private float maxWidth = 500f;
+    private float maxHeight = 500f;
+
+    public bool LimitWidth
+    {
+        get
+        {
+            return limitWidth;
+        }
+        set
+        {
+            limitWidth = value;
+            ForceRebuild();
+        }
+    }
+
+    public float MaxWidth
+    {
+        get
+        {
+            return maxWidth;
+        }
+        set
+        {
+            maxWidth = value;
+            ForceRebuild();
+        }
+    }
+
+    public bool LimitHeight
+    {
+        get
+        {
+            return limitHeight;
+        }
+        set
+        {
+            limitHeight = value;
+            ForceRebuild();
+        }
+    }
+
+    public float MaxHeight
+    {
+        get
+        {
+            return maxHeight;
+        }
+        set
+        {
+            maxHeight = value;
+            ForceRebuild();
+        }
+    }
+
+    private RectTransform rect;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rect = GetComponent<RectTransform>();
+    }
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        ForceRebuild();
+    }
+
+    private void ForceRebuild()
+    {
+        SetLayoutHorizontal();
+        SetLayoutVertical();
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+        base.SetLayoutHorizontal();
+        LimitSize();
+    }
+
+    public override void SetLayoutVertical()
+    {
+        base.SetLayoutVertical();
+        LimitSize();
+    }
+
+    public void LimitSize()
+    {
+        if (limitWidth && rect.rect.width > maxWidth)
+            RefreshWidth();
+
+        if (limitHeight && rect.rect.height > maxHeight)
+            RefreshHeight();
+    }
+
+    public void RefreshWidth()
+    {
+        rect.sizeDelta = new Vector2(maxWidth, rect.sizeDelta.y);
+    }
+
+    public void RefreshHeight()
+    {
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x, maxHeight);
+    }
+
+}
---
+++ Scripts/Editor/ContentSizeFitterWithLimitEditor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(ContentSizeFitterWithLimit))]
+public class ContentSizeFitterWithLimitEditor : Editor
+{
+    ContentSizeFitterWithLimit contentFitter;
+
+    void Prepare()
+    {
+        contentFitter = (ContentSizeFitterWithLimit)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Prepare();
+
+        EditorGUI.BeginChangeCheck();
+        var limitWidth = EditorGUILayout.Toggle("Limit Width", contentFitter.LimitWidth);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(contentFitter, "Modify ContentSizeFitterWithLimit");
+            contentFitter.LimitWidth = limitWidth;
+        }
+
+        if (limitWidth)
+        {
+            EditorGUI.indentLevel++;
+
+            EditorGUI.BeginChangeCheck();
+            var maxWidth = EditorGUILayout.FloatField("Max Width", contentFitter.MaxWidth);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(contentFitter, "Modify ContentSizeFitterWithLimit");
+                contentFitter.MaxWidth = maxWidth;
+            }
+
+            EditorGUI.indentLevel--;
+        }
+
+        EditorGUI.BeginChangeCheck();
+        var limitHeight = EditorGUILayout.Toggle("Limit Height", contentFitter.LimitHeight);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(contentFitter, "Modify ContentSizeFitterWithLimit");
+            contentFitter.LimitHeight = limitHeight;
+        }
+
+        if (limitHeight)
+        {
+            EditorGUI.indentLevel++;
+
+            EditorGUI.BeginChangeCheck();
+            var maxHeight = EditorGUILayout.FloatField("Max Height", contentFitter.MaxHeight);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(contentFitter, "Modify ContentSizeFitterWithLimit");
+                contentFitter.MaxHeight = maxHeight;
+            }
+
+            EditorGUI.indentLevel--;
+        }
+
+    }
+}
